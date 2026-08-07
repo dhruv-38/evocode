36f8ff67857e33c72f45c40bcf17aaecc6ac604a
Dhruv Choudhary <dhruvchoudhary38@gmail.com>
2026-08-07 05:51:00 +0000

    small input command line promt done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::env;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Message {
+    role: String,
+    content: String,
+}
 fn main() {
-    println!("Hello, world!");
+    match env::var("OPENAI_API_KEY") {
+        Ok(_key) => {
+            let prompt: Vec<String> = env::args().collect();
+
+            let message = Message {
+                role: String::from("user"),
+                content: prompt
+                    .get(1)
+                    .unwrap_or(&String::from("No input provided"))
+                    .to_string(),
+            };
+            let json = serde_json::to_string(&message);
+
+            match json {
+                Ok(json) => println!("{json}"),
+                Err(error) => println!("Could not create JSON: {error}"),
+            };
+        }
+        Err(_) => println!("Error: API Key is not set or invalid!"),
+    }
 }
